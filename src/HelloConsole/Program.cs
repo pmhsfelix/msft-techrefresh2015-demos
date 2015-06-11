e3ae5cb54152cd0933917e3a1d4207a3d7271aa7
Pedro Felix <pmhsfelix@gmail.com>
2015-06-11 21:40:42 +0100

    Adds HelloConsole

---
+++ src/HelloConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloConsole
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello console");
+        }
+    }
+}
