de1de9959499e936163000cbc33d884784fc6aef
Pedro Felix <pmhsfelix@gmail.com>
2015-06-15 00:51:33 +0100

    Adds the HelloWebFromMvc sample

---
+++ src/HelloWebFromMvc/Controllers/ResourcesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWebFromMvc.Controllers
+{
+    [Route("api/[controller]")]
+    public class ResourcesController
+    {
+        private readonly HttpRequest _req;
+
+        //private readonly HttpRequest _req;
+
+        [Activate]
+        public ActionContext ActionContext { get; set; }
+
+        public ResourcesController(IHttpContextAccessor ca)
+        {
+            _req = ca.HttpContext.Request;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            //return ActionContext.HttpContext.Request.Headers["Accept"];
+            return "from using contextaccessor " + _req.Headers["Accept"];
+        }
+    }
+}
---
+++ src/HelloWebFromMvc/MyApplicationModelConvention.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.Mvc.ApplicationModels;
+using Microsoft.Framework.Internal;
+
+namespace HelloWebFromMvc
+{
+    internal class MyApplicationModelConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            
+        }
+    }
+}
