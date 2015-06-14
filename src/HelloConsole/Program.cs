067a1b744a081ffdf1531611c242611863ab8348
Pedro Felix <pmhsfelix@gmail.com>
2015-06-15 00:53:12 +0100

    Adds minor edits

--- src/HelloConsole/Program.cs
+++ src/HelloConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Framework.Runtime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +8,17 @@ namespace HelloConsole
 {
     public class Program
     {
+        private readonly IServiceProvider _services;
+
+        public Program(IServiceProvider services)
+        {
+            _services = services;
+        }
+
         public void Main(string[] args)
         {
-            Console.WriteLine("Hello console");
+            var env = _services.GetService(typeof(IApplicationEnvironment)) as IApplicationEnvironment;
+            Console.WriteLine("Hello console, {0}, {1}",env.Configuration, env.RuntimeFramework.FullName);
         }
     }
 }
--- src/HelloWebFromVs/Startup.cs
+++ src/HelloWebFromVs/Startup.cs
@@ -15,16 +15,17 @@ namespace HelloWebFromVs
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<object, object>();
         }
 
         public void Configure(IApplicationBuilder app, ILoggerFactory factory)
         {
             factory.AddConsole();
-            app.UseWelcomePage();
-            //app.Run(async (context) =>
-            //{                
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            //app.UseWelcomePage();
+            app.Run(async (context) =>
+            {                
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
